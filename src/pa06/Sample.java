c169b365fbc186e921105f89ca51084253a08d9d
elliot-siegel <62267188+elliot-siegel@users.noreply.github.com>
2020-04-15 16:54:34 -0400

    Included code from class

--- src/pa06/Sample.java
+++ src/pa06/Sample.java
@@ -1,5 +1,14 @@
 package pa06;
 
 public class Sample {
+  ArrayList<Double> sample;
+	
+	public Sample(double[] values) {
+		this.sample = new ArrayList<Double>();
+		for (int i=0; i<values.length; i++) {
+			sample.add(values[i]);
+		}
+		
+	}
 
 }
