d6cdc3179d3692fc218c321705f2a937a2e41552
elliot-siegel <elliotsiegel@brandeis.edu>
2020-04-15 17:42:51 -0400

    Fixed a few errors

--- src/pa06/Sample.java
+++ src/pa06/Sample.java
@@ -18,35 +18,37 @@ public class Sample {
 		}
 
 	}
+	
+	/** 
+	 * dimension and find distance
+	 */
+	
+	public double distance(double[] p1, double[] p2) {
+		if (p1.length == p2.length) {
+			Double sum = 0D;
+			for (int i = 0; i < p1.length; i++) {
+				double tmp = p2[i] - p1[i];
+				sum = sum + tmp * tmp;
+			}
+			return Math.sqrt(sum);
+		}
+		else {
+			throw new ArithmeticException("Exception in Euclidean distance: array lengths are not equal");
+		}
+	}
+
 
 	public static void main(String[] args) {
 		System.out.println("testing for the Sample class.");
-		double[] p1 = {1d, 2d, 3.14, 2.71};
-		double[] p2 = {0.1, 1.1, 2.1, 3.1};
+		double[] p1 = {2, 2};
+		double[] p2 = {0, 0};
 		Sample s1 = new Sample(p1);
 		Sample s2 = new Sample(p2);
 		System.out.println("s1="+s1);
 		System.out.println("s2="+s2);
-
-		/** dimension and find distance
-*/
-		public double distance(double[] p1, double[] p2) throws Exception {
-				if (p1.length == p2.length) {
-					Double sum = 0D;
-					for (int i = 0; i < p1.length; i++) {
-						double tmp = p2[i] - p1[i];
-						sum = sum + tmp * tmp;
-					}
-					return sum;
-				}
-				else {
-					throw new Exception("Exception in Euclidean distance: array lengths are not equal");
-				}
-			}
+		double value = s2.distance(p1, p2);
+		System.out.println(value);
 
 		}
 
 }
-/** You seemed leave.So I left the meeting~ I don't know whether this is correct.
-please help me check it!
-*/
