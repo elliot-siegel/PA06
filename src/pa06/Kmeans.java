147e86e1ac0630611796c88801fd8c8b06c6ccef
elliot-siegel <elliotsiegel@brandeis.edu>
2020-04-16 17:41:42 -0400

    Edited and commented

--- src/pa06/Kmeans.java
+++ src/pa06/Kmeans.java
@@ -6,25 +6,43 @@ import java.util.Scanner;
 import java.io.File;
 
 public class Kmeans{
+	
+	private Cluster[] clusters;
+	private Cluster originalData;
+	
+	public Kmeans(int numClusters) {
+		// initialize clusters and originalData
+	}
 
+  public void dataCheck(String fileName) throws FileNotFoundException {
 
-
-
-
-
-  public DataCheck(String args[]) {
-    Scanner console = new Scanner(System.in);
-    int numcluaster = 0;
-    private Cluster[] clusters;
-    clusters = null;
       try
     {
-      reader = new Scanner(new File(fileName));
+      Scanner reader = new Scanner(new File(fileName));
 
-      for (numcluaster = 0; reader.readLine() != null; numcluaster++);
+      while(reader.hasNext()) {
+    	  
+    	  //Use reader.nextLine() to get a line of the file
+    	  //Create a new Scanner(reader.nextLine) to read that line
+    	  //Loop through it in another while loop, add each nextInt to an array of doubles
+    	  //Create a new sample with that array, then add the sample to a cluster
+    	  
+      }
 
       reader.close();
-      reader = new Scanner(new File(fileName));
-
+      
+    } catch(FileNotFoundException e) {
+    	System.out.println("Data file not found");
+    }
   }
+  public static void main(String[] args) {
+		Scanner userInput = new Scanner(System.in);
+		System.out.print("Data file name: ");
+		String fileName = userInput.next();
+		System.out.print("Number of clusters: ");
+		int numClusters = userInput.nextInt();
+		
+		// create new Kmeans(numClusters) and then use dataCheck to read the data
+		
+	}
 }
