95205f5f4fa5db95c3fb4b4d11c3c786b03bfc99
elliot-siegel <elliotsiegel@brandeis.edu>
2020-04-16 16:30:43 -0400

    Added getRandomPt

--- src/pa06/Cluster.java
+++ src/pa06/Cluster.java
@@ -11,4 +11,16 @@ public class Cluster {
 	public Cluster() {
 		samples = new ArrayList<Sample>();
 	}
+	
+	public void addSample(Sample s) {
+		this.samples.add(s);
+	}
+	
+	public Sample getRandomPt() {
+		Random rand = new Random();
+		int index = rand.nextInt(this.samples.size() - 1);
+		Sample s = this.samples.get(index);
+		this.clusterPt = s;
+		return s;
+	}
 }
