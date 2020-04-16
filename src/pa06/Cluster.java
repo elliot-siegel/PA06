7546c3a690767067aaef3e1ce7c3494d4b680f40
elliot-siegel <elliotsiegel@brandeis.edu>
2020-04-16 16:17:18 -0400

    Created cluster class

---
+++ src/pa06/Cluster.java
@@ -0,0 +1,14 @@
+package pa06;
+
+import java.util.Random;
+import java.util.ArrayList;
+
+public class Cluster {
+	
+	ArrayList<Sample> samples;
+	Sample clusterPt;
+	
+	public Cluster() {
+		samples = new ArrayList<Sample>();
+	}
+}
