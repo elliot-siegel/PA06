a3077c3e48a8af3cf76125b2486ce080315bb2ab
Jiayao1026 <44878276+Jiayao1026@users.noreply.github.com>
2020-04-16 17:13:08 -0400

    Add files via upload

---
+++ src/pa06/Kmeans.java
@@ -0,0 +1,30 @@
+package pa06;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.File;
+
+public class Kmeans{
+
+
+
+
+
+
+  public DataCheck(String args[]) {
+    Scanner console = new Scanner(System.in);
+    int numcluaster = 0;
+    private Cluster[] clusters;
+    clusters = null;
+      try
+    {
+      reader = new Scanner(new File(fileName));
+
+      for (numcluaster = 0; reader.readLine() != null; numcluaster++);
+
+      reader.close();
+      reader = new Scanner(new File(fileName));
+
+  }
+}
