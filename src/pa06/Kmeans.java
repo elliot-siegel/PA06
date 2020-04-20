55900cd877e7e2c5524acd7e525d60b11c5ada0b
Jiayao1026 <44878276+Jiayao1026@users.noreply.github.com>
2020-04-20 17:03:14 -0400

    Add files via upload
    step 6

--- src/pa06/Kmeans.java
+++ src/pa06/Kmeans.java
@@ -15,11 +15,11 @@ public class Kmeans{
 	  this.clusters = new Cluster[numClusters];
 	  this.originalData = new Cluster();
 	  Sample s = new Sample();
-	  
+
 	  //add 0's to the first sample according to the dimension of the data
 	  for (int i = 0; i < dimension; i++) s.addValue(0);
 	  this.originalData.addSample(s);
-	  
+
 	  this.dimension = dimension;
   }
 
@@ -30,11 +30,11 @@ public class Kmeans{
      Scanner reader = new Scanner(new File(fileName));
 
      while(reader.hasNext()) {
-    	 
+
     	 Sample objectCluster = new Sample();
-    	 
+
     	 for (int i = 0; i < dimension; i++) objectCluster.addValue(reader.nextDouble());
-    	 
+
     	 originalData.addSample(objectCluster);
 
      }
@@ -45,7 +45,7 @@ public class Kmeans{
      System.out.println("Data file not found");
    }
  }
-  
+
  public static void main(String[] args) {
    Scanner userInput = new Scanner(System.in);
    System.out.println("Data file name: ");
@@ -57,12 +57,41 @@ public class Kmeans{
 
    // create new Kmeans(numClusters) and then use dataCheck to read the data
    Kmeans kmeans = new Kmeans(numClusters, dimension);
-   
+
    try {
 	   kmeans.dataCheck(fileName);
    } catch(FileNotFoundException e) {
 	   System.out.println("Data file not found");
    }
-   
+
  }
+}
+   //public static void main(String[] args) {
+
+     //KMeans kmeans = new KMeans();
+     //kmeans.origin();
+     //kmeans.calculate();
+   //}
+
+   //sample/cluster
+//step 6
+   public int[][] Closestsample(int[][] points, int K) {
+        int N = points.length;
+        int[] distance = new int[N];
+        for (int i = 0; i < N; ++i)
+            distance[i] = dist(points[i]);
+
+        Arrays.sort(distance);
+        int distanceofK = distance[K-1];
+
+        int[][] ans = new int[K][2];
+        int t = 0;
+        for (int i = 0; i < N; ++i)
+            if (dist(points[i]) <= distanceofK)
+                ans[t++] = points[i];
+        return ans;
+    }    
+    public int dist(int[] point) {
+        return point[0] * point[0] + point[1] * point[1];
+    }
 }
